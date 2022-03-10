47d1a042347568a565945f9d9c250eb4ea153619
AjithAmin004 <ajithsowda33@gmail.com>
2022-03-10 21:14:59 +0530

    os module intro

---
+++ os.js
@@ -0,0 +1,8 @@
+let os= require('os');
+
+// console.log(os);
+console.log(os.arch());
+console.log(os.cpus());
+console.log(os.freemem());
+console.log(os.getPriority());
+console.log(os.totalmem());
